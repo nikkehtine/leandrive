0a8bf45ea50887c075b49be5bdca2788fc8d8ab3
nikkehtine <nikkehtine@int.pl>
2025-02-02 23:04:55 +0100

    Add loads of stuff

---
+++ src/lib/components/EntityCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<div
+    class="block cursor-pointer select-none text-ellipsis rounded-lg border bg-card p-4 hover:bg-secondary/50 hover:underline"
+>
+    <h2>{name}</h2>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,27 @@
-<div class="min-h-screen bg-neutral-950 text-neutral-100">
-    <h1 class="text-3xl font-bold">Hello nerds</h1>
+<script lang="ts">
+    import { mockFiles, mockFolders } from "$lib/mock-data";
+    import EntityCard from "$lib/components/EntityCard.svelte";
+
+    let currentFolder = $state("root");
+
+    const getCurrentFiles = () => {
+        return mockFiles.filter((file) => file.parentId === currentFolder);
+    };
+    const getCurrentFolders = () => {
+        return mockFolders.filter((folder) => folder.parentId === currentFolder);
+    };
+</script>
+
+<div class="min-h-screen bg-background px-4 text-foreground">
+    <h1 class="py-3 text-3xl font-bold">
+        {currentFolder}
+    </h1>
+    <div class="grid h-full grid-cols-4 gap-2">
+        {#each getCurrentFolders() as folder}
+            <EntityCard name={folder.name} />
+        {/each}
+        {#each getCurrentFiles() as file}
+            <EntityCard name={file.name} />
+        {/each}
+    </div>
 </div>
